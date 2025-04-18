b7366bfb81e824aa57446f51892a8525a04dff5d
lisaosinowo <liseozzy@hotmail.co.uk>
2025-04-18 23:45:18 +0100

    Added the navbar and the router to navigate between different sections/pages

---
+++ Final_ReactApp/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import '../styles/navbar.css';
+import moodieLogo from '../assets/moodielogo.png'
+
+const Navbar = () => {
+   const navigate = useNavigate();
+
+   const startMoodie = () => {
+      navigate("/startmoodie"); // This takes the user to the Start Moodie page when they press the button
+   };
+
+   return (
+      <>
+         
+         <div className="nav-container">
+            <img id='moodie-logo' src={moodieLogo} />
+            <nav>
+               <ul>
+                  <li>
+                     <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                     <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                     <Link to="/thecreators">The Creators</Link>
+                  </li>
+                  <button id="start-moodie-btn" onClick={startMoodie}>Start Moodie</button>
+               </ul>
+            </nav>
+         </div>
+         
+      </>
+   );
+};
+
+export default Navbar;
--- Final_ReactApp/src/main.jsx
+++ Final_ReactApp/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import StartMoodie from './pages/StartMoodie.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Navbar /> 
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/startmoodie' element={<StartMoodie />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
