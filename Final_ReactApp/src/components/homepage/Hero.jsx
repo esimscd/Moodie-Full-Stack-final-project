50b1205fa879d09f17b8bde85ec55aae065c62b7
lisaosinowo <liseozzy@hotmail.co.uk>
2025-04-28 18:35:57 +0100

    Changed the homepage into components

---
+++ Final_ReactApp/src/components/homepage/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../styles/homepage.css";
+import HeroImg from "../../assets/heroimage.png";
+import Camera from '../../assets/camera.png'
+
+const Hero = () => {
+   return (
+      <>
+         <div id="hero-section" className="hero-container">
+            <div className="left-container">
+               <img src={HeroImg} />
+               <p className="hero-slogan">Let your mood pick the movie</p>
+            </div>
+            <div className="right-container">
+               <img src={Camera} />
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default Hero;
