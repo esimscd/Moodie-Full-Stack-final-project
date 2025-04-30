1f08878f07ace72dac9b74e395af718a05676e46
dannishuttleworth <124736568+dannishuttleworth@users.noreply.github.com>
2025-04-30 23:45:00 +0100

    Centres the question box

--- Final_ReactApp/src/components/QuizAnswerButton.jsx
+++ Final_ReactApp/src/components/QuizAnswerButton.jsx
@@ -3,12 +3,12 @@ import "../styles/QuizAnswerButton.css";
 
 const QuizAnswerButton = ({ quizAnswer, onClick }) => {
   return (
-    <div className="quiz-answer-button-container">
+    <>
       <button className="quiz-answer-button" onClick={onClick}>
         {/* This button will be dynamically generated based on the quiz question */}
         {quizAnswer}
       </button>
-    </div>
+    </>
   );
 };
 
