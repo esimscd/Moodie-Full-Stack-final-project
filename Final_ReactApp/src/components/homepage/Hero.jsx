cebb40315d3ca4a0cb82315078933b72ddc5a7e9
lisaosinowo <liseozzy@hotmail.co.uk>
2025-05-01 13:15:06 +0100

    Added the mobile/tablet section to the hero component. Made the hero section fully responsive

--- Final_ReactApp/src/components/homepage/Hero.jsx
+++ Final_ReactApp/src/components/homepage/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../styles/homepage.css";
 import HeroImg from "../../assets/heroimage.png";
-import Camera from '../../assets/camera.png'
+import Camera from "../../assets/camera.png";
+import Popcorn from "../../assets/logos/moodie-popcorn.png";
 
 const Hero = () => {
    return (
       <>
-         
          <div id="hero-section" className="hero-container">
             <div className="left-container">
                <img src={HeroImg} />
@@ -14,6 +14,18 @@ const Hero = () => {
             </div>
             <div className="right-container">
                <img src={Camera} />
+            </div>
+            <div className="mobile-tablet-hero-container">
+               <div className="popcorn-img-container">
+                  <img className="popcorn-bg" src={Popcorn} alt="" />
+               </div>
+               
+               
+               <div className="mobile-hero-content">
+                  <img src={HeroImg} />
+                  <p className="hero-slogan">Let your mood pick the movie</p>
+               </div>
+               
             </div>
          </div>
       </>
