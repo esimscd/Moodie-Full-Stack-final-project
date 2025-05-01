a2f586c803e58851317ccc6c256b41f69f3df54e
lisaosinowo <liseozzy@hotmail.co.uk>
2025-05-01 11:09:18 +0100

    Separated the question image from the question text container, put the image and question text in separate containers

--- Final_ReactApp/src/components/QuizQuestion.jsx
+++ Final_ReactApp/src/components/QuizQuestion.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import "../styles/QuizQuestion.css";
+import Danni from "../assets/TeamAvatars/Danni.png";
 
 const QuizQuestion = ({ question, image }) => {
-  return (
-    <div className="quiz-question-box">
-      <h1 className="question-wording">{question}</h1>
-      {/* Conditionally render the image if image is provided as part of the question */}
-      {image && (
-        <div className="question-image">
-          <img src={image} alt="question image" />
-        </div>
-      )}
-    </div>
-  );
+   return (
+      <>
+         <div className="quiz-question-container">
+            <div className="quiz-content-container">
+               <div className="quiz-question-box">
+                  <h1 className="question-wording">{question}</h1>
+                  {/* Conditionally render the image if image is provided as part of the question */}
+               </div>
+            </div>
+            <div className="image-container">
+               {image && (
+                  <div className="question-image">
+                     <img src={image} alt="question image" />
+                  </div>
+               )}
+               <div className="question-image">
+                  <img id="danni" src={Danni} alt="" />{" "}
+                  {/* This is a placeholder for the real images */}
+               </div>
+            </div>
+         </div>
+      </>
+   );
 };
 
 export default QuizQuestion;
