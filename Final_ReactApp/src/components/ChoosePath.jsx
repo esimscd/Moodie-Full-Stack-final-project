b8e6b736aac57735dda0288fa716df0f090c2a82
Alissa Monova <alissamakesartthings@gmail.com>
2025-05-04 01:53:30 +0100

    update quiz questions, ensured correct css import

--- Final_ReactApp/src/components/ChoosePath.jsx
+++ Final_ReactApp/src/components/ChoosePath.jsx
@@ -5,6 +5,7 @@ import "../styles/StartMoodieChoosePath.css";
 function ChoosePath() {
   const navigate = useNavigate();
 
+  // path parameter navigates to route or page specified in button element
   const handleClick = (path) => {
     navigate(path);
   };
